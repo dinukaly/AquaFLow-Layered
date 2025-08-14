ce6d0fb0dafcd3d5be8fca372c6f3c9b06938653
Dinuka Liyanage <infodinukalk@gmail.com>
2025-08-14 13:36:12 +0530

    implement missing methods

--- src/main/java/lk/wms/aquaflow/bo/custom/impl/ComplaintsBOImpl.java
+++ src/main/java/lk/wms/aquaflow/bo/custom/impl/ComplaintsBOImpl.java
@@ -1,7 +1,64 @@
 package lk.wms.aquaflow.bo.custom.impl;
 
 import lk.wms.aquaflow.bo.custom.ComplaintsBO;
+import lk.wms.aquaflow.dto.ComplaintDTO;
+
+import java.util.ArrayList;
 
 public class ComplaintsBOImpl implements ComplaintsBO {
 
+    @Override
+    public String getNextComplaintId() {
+        return "";
+    }
+
+    @Override
+    public boolean addComplaint(ComplaintDTO complaintDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean updateComplaint(ComplaintDTO complaintDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteComplaint(String complaintId) {
+        return false;
+    }
+
+    @Override
+    public boolean updateComplaintStatus(String complaintId, String status) {
+        return false;
+    }
+
+    @Override
+    public ComplaintDTO getComplaintById(String complaintId) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<ComplaintDTO> getAllComplaints() {
+        return null;
+    }
+
+    @Override
+    public int getTotalComplaintsCount() {
+        return 0;
+    }
+
+    @Override
+    public int getSolvedComplaintsCount() {
+        return 0;
+    }
+
+    @Override
+    public int getScheduledComplaintsCount() {
+        return 0;
+    }
+
+    @Override
+    public int getNewComplaintsThisMonth() {
+        return 0;
+    }
 }
