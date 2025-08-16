275dc4c11be701b1bebd677a0b4a041b6cabd80c
Dinuka Liyanage <infodinukalk@gmail.com>
2025-08-17 03:39:02 +0530

    change name

--- src/main/java/lk/wms/aquaflow/AppInitializer.java
+++ src/main/java/lk/wms/aquaflow/AppInitializer.java
@@ -4,12 +4,9 @@ package lk.wms.aquaflow;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-public class HelloApplication extends Application {
+public class AppInitializer extends Application {
 
 
     public static void main(String[] args) {
