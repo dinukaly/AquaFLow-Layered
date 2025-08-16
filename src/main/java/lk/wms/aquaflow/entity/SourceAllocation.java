a511d3f867b9e57e8e574fe655fea2cdffaa2275
Dinuka Liyanage <infodinukalk@gmail.com>
2025-08-17 03:35:34 +0530

    data type issue fix

--- src/main/java/lk/wms/aquaflow/entity/SourceAllocation.java
+++ src/main/java/lk/wms/aquaflow/entity/SourceAllocation.java
@@ -12,7 +12,7 @@ import java.sql.Date;
 @AllArgsConstructor
 public class SourceAllocation {
     private String allocationId;
-    private BigDecimal allocationAmount;
+    private double allocationAmount;
     private String watersourceId; // FK
     private String villageId; // FK
     private Date allocationDate;
