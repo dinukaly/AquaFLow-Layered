e241263c6d6d80e071276b3ecfa70a510ec67c88
Dinuka Liyanage <infodinukalk@gmail.com>
2025-08-17 03:37:02 +0530

    project completed

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,11 +3,26 @@ module lk.wms.aquaflow {
     requires javafx.fxml;
     requires java.mail;
     requires javafx.media;
+    requires java.sql;
+    requires static lombok;
+    requires mysql.connector.j;
+    requires net.sf.jasperreports.core;
 
 
     opens lk.wms.aquaflow to javafx.fxml;
+    opens lk.wms.aquaflow.controller to javafx.fxml;
+    opens lk.wms.aquaflow.controller.modal to javafx.fxml;
     exports lk.wms.aquaflow;
     exports lk.wms.aquaflow.controller;
+    exports lk.wms.aquaflow.entity;
+    exports lk.wms.aquaflow.dao;
+    exports lk.wms.aquaflow.dao.custom;
+    exports lk.wms.aquaflow.bo;
+    exports lk.wms.aquaflow.bo.custom;
+    exports lk.wms.aquaflow.bo.custom.impl;
+    exports lk.wms.aquaflow.dto;
+    exports lk.wms.aquaflow.util;
+    exports lk.wms.aquaflow.view.tm;
     exports lk.wms.aquaflow.controller.modal;
 
 }
