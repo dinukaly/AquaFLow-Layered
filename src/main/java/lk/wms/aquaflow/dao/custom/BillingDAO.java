21755b56c8620e554d2a3439d8c965fd7b7f324b
Dinuka Liyanage <infodinukalk@gmail.com>
2025-08-17 03:17:39 +0530

    implement dao interfaces

--- src/main/java/lk/wms/aquaflow/dao/custom/BillingDAO.java
+++ src/main/java/lk/wms/aquaflow/dao/custom/BillingDAO.java
@@ -3,5 +3,16 @@ package lk.wms.aquaflow.dao.custom;
 import lk.wms.aquaflow.dao.CrudDAO;
 import lk.wms.aquaflow.entity.Bill;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public interface BillingDAO extends CrudDAO<Bill> {
+    boolean updateStatus(String billId, String status) throws SQLException, ClassNotFoundException;
+    List<Bill> getByStatus(String status) throws SQLException, ClassNotFoundException;
+    List<Bill> getOverdueBills() throws SQLException, ClassNotFoundException;
+
+    double getTotalCollectedAmount() throws SQLException, ClassNotFoundException;
+    double getPendingPaymentAmount() throws SQLException, ClassNotFoundException;
+    double getOverduePaymentAmount() throws SQLException, ClassNotFoundException;
+    double getTotalBilledThisMonth() throws SQLException, ClassNotFoundException;
 }
